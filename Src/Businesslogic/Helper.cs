274c8aed32d5459c81b7a352218cfc35331166ad
Ferdi <ferdivt@deltan-n.nl>
2023-12-01 10:03:59 +0100

    Init commit

---
+++ Src/Businesslogic/Helper.cs
@@ -0,0 +1,42 @@
+﻿using Businesslogic.Attributes;
+using Businesslogic.Enums;
+using Businesslogic.Extensions;
+using Pastel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Businesslogic
+{
+    public static class Helper
+    {
+        public static List<string> GetFileContents(FileType fileType)
+        {
+            var filename = fileType.GetAttributeOfType<FileNameAttribute>().FileName;
+            var text = System.IO.File.ReadAllText(filename);
+            return text.Split(Environment.NewLine).ToList();
+        }
+        public static void WriteResult(Func<List<string>,int> func, FileType fileType, int result)
+        {
+            WriteResult((x) => func(x).ToString(), fileType, result.ToString());
+        }
+
+        public static void WriteResult(Func<List<string>, string> func, FileType fileType, string result)
+        {
+            var result1Test = func(GetFileContents(fileType));
+            Console.WriteLine($"Result of {fileType} is: {result1Test.Pastel(Color.Red)}");
+            var resultString = new StringBuilder();
+            resultString.Append(result).Append(" == ").Append(result1Test);
+            resultString.Append(result == result1Test ? "CORRECT".Pastel(Color.Green) : "INCORRECT".Pastel(Color.Red));
+
+            Console.WriteLine($"Result of {fileType} is: {resultString}");
+        }
+
+        public static void WriteResult(Func<List<string>, long> func, FileType fileType, int result)
+        {
+            WriteResult((x) => func(x).ToString(), fileType, result.ToString());
+        }
+    }
+}
---
+++ Src/Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Businesslogic;
+using Businesslogic.Extensions;
+using Businesslogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using Pastel;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper.WriteResult(Test1, FileType.Test1Sample);
+            //Helper.WriteResult(Test1, FileType.Test1);
+            //Helper.WriteResult(Test2, FileType.Test2Sample);
+            //Helper.WriteResult(Test2, FileType.Test2);
+        }
+
+        private static int Test1(List<string> input)
+        {
+            throw new NotImplementedException();
+            return 0;
+        }
+
+        private static int Test2(List<string> input)
+        {
+            throw new NotImplementedException();
+            return 0;
+        }
+    }
+}
